8148fce50d473476e66926913a10c0ca08bda09f
Naftali Berger <bn26665@gmail.com>
2025-05-19 16:24:01 +0300

    furst fanc

--- Desktop/הקדמה לפייתון/oop2/oop2/oop_ex_basic.cs
+++ Desktop/הקדמה לפייתון/oop2/oop2/oop_ex_basic.cs
@@ -1,1 +1,20 @@
-﻿
+﻿public class Tool
+{
+    public string name;
+    public string weight;
+
+    public Tool(string name, string weight)
+    {
+        this.name = name;
+        this.weight = weight;
+    }
+    public abstract Describe();
+
+
+
+
+
+}
+
+
+
